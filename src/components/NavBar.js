772201b4313d7fa66d3d2b9ca12a88910f44a84b
jasti lakshith <119104932+jastilakshith@users.noreply.github.com>
2025-04-30 23:08:09 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 export default function NavBar() {
+    const location = useLocation();
   return (
     <div>
         <nav>
-          <Link className='linktag' to={'/about'}>About</Link>
-          <Link className='linktag' to={'/skills'}>Skills</Link>
-          <Link className='linktag' to={'/hobbies'}>Hobbies and Interests</Link>
-          <Link className='linktag' to={'/academicbackground'}>Aacdemic background</Link>
-          <Link className='linktag' to={'/passion'}>Passion</Link>
-          <Link className='linktag' to={'/certifications'}>Certifications</Link>
+          <Link className={`linktags a ${location.pathname.replace('/', '')}`} to={'/about'}>About</Link>
+          <Link className={`linktags b ${location.pathname.replace('/', '')}`} to={'/skills'}>Skills</Link>
+          <Link className={`linktags c ${location.pathname.replace('/', '')}`} to={'/hobbies'}>Hobbies and Interests</Link>
+          <Link className={`linktags d ${location.pathname.replace('/', '')}`} to={'/academicbackground'}>Aacdemic background</Link>
+          <Link className={`linktags e ${location.pathname.replace('/', '')}`} to={'/passion'}>Passion</Link>
+          <Link className={`linktags f ${location.pathname.replace('/', '')}`} to={'/certifications'}>Certifications</Link>
         </nav>
     </div>
   )
