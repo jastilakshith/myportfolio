d32f8b06c42bf2096909dd711684e1b312e8775f
jasti lakshith <119104932+jastilakshith@users.noreply.github.com>
2025-04-30 21:12:12 +0530

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,6 +2,36 @@ import React from 'react'
 
 export default function Skills() {
   return (
-    <div>Skills</div>
+    <div className='skillscontainer'>
+        <div className="section">
+            <div className="tiles">
+                <h1>Java</h1>
+            </div>
+            <div className="tiles">
+                <h1>C</h1>
+            </div>
+            <div className="tiles">
+                <h1>ReactJS</h1>
+            </div>
+            <div className="tiles">
+                <h1>SQL</h1>
+            </div>
+        </div>
+
+        <div className="section">
+            <div className="tiles">
+                <h1>JavaScript</h1>
+            </div>
+            <div className="tiles">
+                <h1>C++</h1>
+            </div>
+            <div className="tiles">
+                <h1>NodeJS</h1>
+            </div>
+            <div className="tiles">
+                <h1>MongoDB</h1>
+            </div>
+        </div>
+    </div>
   )
 }
